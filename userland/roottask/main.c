b925e23859b3c86ffc29e285f89b0572d4ea97b2
julia <midnight@trainwit.ch>
2026-01-10 20:42:27 +1100

    print bootinfo from kernel
    Signed-off-by: julia <midnight@trainwit.ch>

--- userland/roottask/main.c
+++ userland/roottask/main.c
@@ -1,5 +1,58 @@
 #include <sel4/sel4.h>
+#include <stdint.h>
 
-int main(void) {
-    seL4_DebugPutString("hello\n");
+static inline char hexchar(unsigned int v) {
+    return v < 10 ? '0' + v : ('a' - 10) + v;
+}
+
+#define puts seL4_DebugPutString
+
+void puthex32(uint32_t val) {
+    char buffer[8 + 3];
+    buffer[0] = '0';
+    buffer[1] = 'x';
+    buffer[8 + 3 - 1] = 0;
+    for (unsigned i = 8 + 1; i > 1; i--) {
+        buffer[i] = hexchar(val & 0xf);
+        val >>= 4;
+    }
+
+    puts(buffer);
+}
+
+void start(seL4_BootInfo *bootinfo) {
+    puts("hello\n");
+    puthex32((uint32_t)bootinfo);
+
+    puts("\nseL4_BootInfo");
+    puts("\n  extraLen: "); puthex32(bootinfo->extraLen);
+    puts("\n  nodeID: "); puthex32(bootinfo->nodeID);
+    puts("\n  numNodes: "); puthex32(bootinfo->numNodes);
+    puts("\n  numIOPTLevels: "); puthex32(bootinfo->numIOPTLevels);
+    puts("\n  ipcBuffer: "); puthex32((uint32_t)bootinfo->ipcBuffer);
+
+    puts("\n  empty: "); puthex32(bootinfo->empty.start); puts(".."); puthex32(bootinfo->empty.end);
+    puts("\n  sharedFrames: "); puthex32(bootinfo->sharedFrames.start); puts(".."); puthex32(bootinfo->sharedFrames.end);
+    puts("\n  userImageFrames: "); puthex32(bootinfo->userImageFrames.start); puts(".."); puthex32(bootinfo->userImageFrames.end);
+    puts("\n  userImagePaging: "); puthex32(bootinfo->userImagePaging.start); puts(".."); puthex32(bootinfo->userImagePaging.end);
+    puts("\n  ioSpaceCaps: "); puthex32(bootinfo->ioSpaceCaps.start); puts(".."); puthex32(bootinfo->ioSpaceCaps.end);
+    puts("\n  extraBIPages: "); puthex32(bootinfo->extraBIPages.start); puts(".."); puthex32(bootinfo->extraBIPages.end);
+
+    puts("\n  initThreadCNodeSizeBits: "); puthex32(bootinfo->initThreadCNodeSizeBits);
+    puts("\n  initThreadDomain: "); puthex32(bootinfo->initThreadDomain);
+
+    puts("\n  untyped: "); puthex32(bootinfo->untyped.start); puts(".."); puthex32(bootinfo->untyped.end);
+    for (uint32_t i = 0; i < CONFIG_MAX_NUM_BOOTINFO_UNTYPED_CAPS; i++) {
+        if (bootinfo->untypedList[i].sizeBits == 0) break;
+
+        puts("\n  untyped[");
+        puthex32(i);
+        puts("] = ");
+        puthex32(bootinfo->untypedList[i].paddr);
+        puts("..");
+        puthex32(bootinfo->untypedList[i].paddr + (1ULL << bootinfo->untypedList[i].sizeBits));
+        puts(bootinfo->untypedList[i].isDevice ? " (is device)" : " (is normal)");
+    }
+
+    puts("\ndone\n");
 }
