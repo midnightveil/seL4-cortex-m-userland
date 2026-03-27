fd492204f4a73e325a526f324882c8d81392c1b0
julia <midnight@trainwit.ch>
2026-03-28 10:42:42 +1100

    add gdb script
    Signed-off-by: julia <midnight@trainwit.ch>

---
+++ gdb-rp2350.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+gdb -ex 'add-symbol-file userland/roottask/roottask.elf' -ex 'add-symbol-file kernel/kernel.elf' -ex 'tar ext :3333' -ex 'monitor reset init' -ex 'load meL4.elf' -ex continue
