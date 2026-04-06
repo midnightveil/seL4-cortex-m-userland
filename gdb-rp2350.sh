b3f0b0b0947414dfd613d3275e5399ab13bf5d6b
julia <midnight@trainwit.ch>
2026-04-06 17:19:10 +1000

    meL4 with systick
    Signed-off-by: julia <midnight@trainwit.ch>

--- gdb-rp2350.sh
+++ gdb-rp2350.sh
@@ -2,4 +2,4 @@
 
 set -eo pipefail
 
-gdb -ex 'add-symbol-file userland/roottask/roottask.elf' -ex 'add-symbol-file kernel/kernel.elf' -ex 'tar ext :3333' -ex 'monitor reset init' -ex 'load meL4.elf' -ex continue
+gdb -ex 'set confirm off' -ex 'add-symbol-file userland/roottask/roottask.elf' -ex 'add-symbol-file kernel/kernel.elf' -ex 'tar ext :3333' -ex 'monitor reset init' -ex 'load meL4.elf' -ex continue
